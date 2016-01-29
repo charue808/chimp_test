3e9c9e492032b9fa39df99d3f7c3c916e878be6d
charue808 <charlesgum@gmail.com>
2016-01-29 13:55:18 -1000

    fourth commit - step definitions

---
+++ features/support/step_defs.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+
+	this.Given(/^I am on Google$/, function () {
+  // Write the automation code here
+  pending();
+	});
+
+	this.When(/^I search for "([^"]*)"$/, function (arg1) {
+  // Write the automation code here
+  pending();
+	});
+
+	this.Then(/^I see "([^"]*)"$/, function (arg1) {
+  // Write the automation code here
+  pending();
+	});
+	
+};
